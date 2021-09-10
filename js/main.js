32ba5957286b768eb50028bdb7a4749c38aa5a3f
Aditya <adityanashikkar94@gmail.com>
2021-09-10 21:04:08 +0530

    qualification & journey completed

--- js/main.js
+++ js/main.js
@@ -29,8 +29,48 @@ function linkAction () {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills-content'),
+  skillsHeader = document.querySelectorAll('.skills-header')
+
+function toggleSkills() {
+ 
+  let itemClass = this.parentNode.className
+
+  for (i=0; i<skillsContent.length; i++) {
+    skillsContent[i].className = 'skills-content skills-close';
+  }
+
+  if (itemClass === 'skills-content skills-close') {
+    this.parentNode.className = 'skills-content skills-open';
+  }
+
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification-active');
+        });
+
+        target.classList.add('qualification-active');
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification-active');
+        })
+        tab.classList.add('qualification-active');
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
