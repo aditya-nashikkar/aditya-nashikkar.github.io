9d06d6cd5964ebf8650cb1acbfc9a82e0aecbdc4
Aditya <adityanashikkar94@gmail.com>
2021-09-13 00:14:37 +0530

    portfolio complete

--- js/main.js
+++ js/main.js
@@ -98,6 +98,19 @@ modalCloses.forEach(modalClose => {
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
+let swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
+
 /*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
